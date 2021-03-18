350d6a26c1574f7324ef430df2a442673a81a791
Joshuascharley <34540432+joshuacharley@users.noreply.github.com>
2021-03-18 10:31:06 -0700

    settingUpChatSection

--- src/App.js
+++ src/App.js
@@ -1,2 +1,23 @@
-import { ChatEngine } from 'react-chat-engine'
-import '.App.css'
+import React from 'react';
+import './App.css'
+
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
+
+
+const App = () => {
+  return (
+    <div>
+    {/* calling ChatEngine as a component and adding its props */}
+      <ChatEngine
+          height="100vh"
+          projectID="bedc7522-d66a-4181-8223-96d55762b394"
+          userName="Edmondcharley"
+          userSecret="199811"
+          renderChatFeed={( afterofficeChatAppProps) => <ChatFeed { ... afterofficeChatAppProps} />}
+      />
+    </div>
+  );
+}
+
+export default App;
